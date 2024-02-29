acfc07520251996264b03f9513a1de54d8d66b5d
shahrudra9829 <rudrashah040@gmail.com>
2024-02-29 23:42:10 +0530

    Display of image in reduces size

--- client/src/component/DefaultLayout.js
+++ client/src/component/DefaultLayout.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+
+
 function DefaultLayout(props) {
 
-    return (
-        <div>
-            <div className="header">
-                <div className="d-flex justify-content-between">
-                    <h1 className="heading">WanderWheels</h1>
-                    <h3>About Us</h3>
-                    <div className='button-container'>
-                        <button>
-                            <Link to='/login'>Login/SignUp</Link>
-                        </button>
+        return (
+            <div>
+                <div className="header">
+                    <div className="d-flex justify-content-between">
+                        
+                        <h1 className="heading">WanderWheels</h1>
+                        <h3>About Us</h3>
+                        <div className='button-container'>
+                            <button>
+                                <Link to='/login'>Login/SignUp</Link>
+                            </button>
+                        </div>
                     </div>
                 </div>
+                <div className="Content">
+                    {props.children}
+                </div>
             </div>
-            <div className="Content">
-                {props.children}
-            </div>
-        </div>
-    )
-}
+        )
+    }
 
-export default DefaultLayout
+    export default DefaultLayout
